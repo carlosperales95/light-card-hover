09cbfe5c99f678c60a8ab7e0da34969e594f9c73
carlosperales95 <carlos.perales95@opendeusto.es>
2023-03-11 21:36:40 +0100

    Added images and overall styling improvement

---
+++ light-hover/src/components/CardContainer.vue
@@ -0,0 +1,27 @@
+<template>
+
+  <div id="cards">
+    <OptionCard title="Events" description="Check what goes on in your neighborhood" filename="event"/>
+    <OptionCard title="Festivals" description="The right mood at the right time" filename="festival" />
+    <OptionCard title="Bars" description="This is my last drink..." filename="bar" />
+    <OptionCard title="Clubs" description="Say bye to your money" filename="club" />
+    <OptionCard title="Piazza Verdi" description="Gathering odore di birkenstock" filename="verdi" />
+    <OptionCard title="Raves" description="Until the popos get us" filename="rave" />
+  </div>
+
+</template>
+
+<script>
+  import OptionCard from './OptionCard.vue';
+
+  export default {
+  name: 'CardContainer',
+  // props: {
+  //   title: String,
+  //   description: Strin
+  // }
+  components: {
+    OptionCard
+  }
+  }
+</script>
---
+++ light-hover/src/components/OptionCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="card">
+    <div class="card-content">
+      <div class="card-image">
+        <img :src="require(`@/assets/${filename}.png`)">
+      </div>
+      <div class="card-info-wrapper">
+        <div class="card-info">
+          <div class="card-info-title">
+            <h3> {{ title }} </h3>  
+            <h4> {{ description }} </h4>
+          </div>    
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+  name: 'OptionCard',
+  props: {
+    title: String,
+    description: String,
+    filename : String
+  }
+  }
+</script>
